2e838ba2e54e6ee0aced89e7118923329e7c7ae7
joelralphdeveloper <163519284+joelralphdeveloper@users.noreply.github.com>
2025-01-15 12:10:46 +0800

    chore: Add profile_picture path in message-inbox endpoint

--- src/controllers/message-inbox.controller.ts
+++ src/controllers/message-inbox.controller.ts
@@ -9,26 +9,26 @@ export default class MessageCtrl {
   static async getMessages(req: Request, res: Response) {
     try {
       const { id, inbox_id, room_id, page = 1, limit = 10 } = req.query as any;
-  
+
       const { error } = validateGetMessages(req.query);
       if (error) {
         return handleErrorResponse(res, error, { code: "VALIDATION_ERROR" });
       }
-  
+
       const pageNumber = parseInt(page.toString());
       const limitNumber = parseInt(limit.toString());
       const offset = (pageNumber - 1) * limitNumber;
-  
+
       const query: any = {
         ...(id && { _id: new ObjectId(id) }),
         ...(inbox_id && { inbox: new ObjectId(inbox_id) }),
         ...(room_id && { room_id }),
         deletedAt: { $eq: null },
       };
-  
+
       const list_count = await MessageSvc.getTotalCountMessage(query);
       const list = await MessageSvc.getMessages(query, offset, limitNumber);
-  
+
       const result = {
         data: list,
         total_pages: Math.ceil(list_count / limitNumber) || 0,
@@ -37,7 +37,7 @@ export default class MessageCtrl {
         size: limitNumber,
         offset,
       };
-  
+
       return handleResponse(res, result, "ENQUIRIES_FETCHED");
     } catch (error) {
       return handleErrorResponse(res, error, { code: "ENQUIRIES_FETCH_FAILED" });
