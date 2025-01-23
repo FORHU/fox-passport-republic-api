62929d09c89beb4daadeb5c37ee022834e4cff41
joelralphdeveloper <163519284+joelralphdeveloper@users.noreply.github.com>
2025-01-23 11:12:34 +0800

    chore: Gogoji change redirection uri for stripe account onboarding

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -3,13 +3,14 @@ import express from "express";
 import PaymentCtl from "../controllers/payment.controller";
 import authenticateToken from "../middleware/authenticate-token.middleware";
 import sessionMiddleware from "../middleware/valid-session.middleware";
+import tenantMiddleware from "../middleware/tenant.middleware";
 
 const router = express.Router();
 
 router.post("/process-payment", [sessionMiddleware, authenticateToken], PaymentCtl.processPayment);
 router.post("/process-payment/status", [sessionMiddleware, authenticateToken], PaymentCtl.processPaymentStatus);
 router.post("/compute", PaymentCtl.computePayment);
-router.post("/create-account", [sessionMiddleware, authenticateToken], PaymentCtl.createAccount);
+router.post("/create-account", [sessionMiddleware, authenticateToken, tenantMiddleware], PaymentCtl.createAccount);
 router.get("/setup-intent", [sessionMiddleware, authenticateToken], PaymentCtl.setupPaymentIntent);
 router.get("/get-payment-details", [sessionMiddleware, authenticateToken], PaymentCtl.getPaymentDetails);
 router.get("/create-login", [sessionMiddleware, authenticateToken], PaymentCtl.createLoginLink);
