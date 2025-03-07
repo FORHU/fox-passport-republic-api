a24484036468dc0a66669809e9086b220b106e44
KevinDaus7365 <kevin@seven365.com.sg>
2025-03-07 22:37:03 +0800

    Create notification.model.ts
    create model file for notification schema.

---
+++ src/models/notification.model.ts
@@ -0,0 +1,68 @@
+import { ObjectId } from "mongodb";
+
+export enum NotificationStatusType {
+  READ = "READ",
+  UNREAD = "UNREAD",
+}
+
+export interface TNotications {
+  _id?: ObjectId;
+  sender: ObjectId;
+  receiver: ObjectId;
+  metadata?: {
+    enquiry_id?: ObjectId;
+    booking_id?: ObjectId;
+  };
+  title?: string;
+  body?: string;
+  status?: NotificationStatusType;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+  deletedBy?: ObjectId;
+}
+
+export class MNotications implements Partial<TNotications> {
+  _id?: ObjectId;
+  sender: ObjectId;
+  receiver: ObjectId;
+  title?: string;
+  metadata?: {
+    enquiry_id?: ObjectId;
+    booking_id?: ObjectId;
+  };
+  body?: string;
+  status?: NotificationStatusType;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+  deletedBy?: ObjectId;
+
+  constructor(
+    {
+      _id = new ObjectId(),
+      sender,
+      receiver,
+      metadata,
+      title,
+      body,
+      status,
+      createdAt = new Date(),
+      updatedAt,
+      deletedAt,
+      deletedBy,
+    }: TNotications = {} as TNotications,
+  ) {
+    this._id = _id;
+    this.sender = sender;
+    this.receiver = receiver;
+    this.metadata = metadata;
+    this.title = title;
+    this.body = body;
+    this.status = status;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+    this.deletedBy = deletedBy;
+  }
+}
