e60d27b99ce32d91aeef1510dffb9b6d22ab14a8
KevinDaus7365 <kevin@seven365.com.sg>
2025-03-07 22:34:39 +0800

    Update config.ts
    add values needed for firebase in config file

--- src/config.ts
+++ src/config.ts
@@ -68,3 +68,6 @@ export const TENANT_ALLOW_REFERER = (process.env.TENANT_ALLOW_REFERER as string)
 export const VENUE4USE_ALLOW_REFERER = process.env.VENUE4USE_ALLOW_REFERER as string;
 export const TENANT_VENUE4USE_API_KEY = process.env.TENANT_VENUE4USE_API_KEY as string;
 export const TENANT_GOGOJI_API_KEY = process.env.TENANT_GOGOJI_API_KEY as string;
+export const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY as string;
+export const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL as string;
+export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID as string;
