f00de1a168bede19985f3ed6a9dfd5f3d7359243
KevinDaus7365 <kevin@seven365.com.sg>
2025-03-13 15:09:03 +0800

    Update notification.repository.ts
    rename misspelled notification word

--- src/repositories/notification.repository.ts
+++ src/repositories/notification.repository.ts
@@ -1,4 +1,4 @@
-import { TNotications, MNotications } from "../models/notification.model";
+import { TNotifications, MNotifications } from "../models/notification.model";
 import { getDB } from "../utils/mongo";
 
 export default class NotificationRepo {
@@ -6,13 +6,40 @@ export default class NotificationRepo {
     return getDB().collection("notifications");
   }
 
-  static async createNotification(data: TNotications) {
+  static async createNotification(data: TNotifications) {
     try {
-      const notifInstance = new MNotications(data);
+      const notifInstance = new MNotifications(data);
       await this.collection().insertOne(notifInstance);
       return notifInstance;
     } catch (error) {
       throw error;
     }
   }
+
+  static async getOneNotification(query: TNotifications) {
+    try {
+      const result = await this.collection().findOne(query);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getUnreadNotificationsCountEnquiries(query: any) {
+    try {
+      const count = await this.collection().countDocuments(query);
+      return count;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // static async updateNotification(query: any, data: any) {
+  //   try {
+  //     const result = await this.collection().updateMany(query, { $set: data });
+  //     return result;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
 }
