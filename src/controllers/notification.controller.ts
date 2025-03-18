b360fcece85603c2908e6b89bc6615c5c8ae8e7b
KevinDaus7365 <kevin@seven365.com.sg>
2025-03-18 14:36:00 +0800

    Update notification.controller.ts
    build code for deleting read notification past 30 days

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -8,6 +8,12 @@ export default class NotificationCtrl {
     try {
       const user = req.user;
       const query = { receiver: new ObjectId(user._id), read: false };
+
+      // const thirtyDaysAgo = new Date();
+      // thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+      // const deleteQuery = { receiver: new ObjectId(user._id), read: true, createdAt: { $lte: thirtyDaysAgo } };
+      // await NotificationSvc.deleteNotifications(deleteQuery);
+
       const result = await NotificationSvc.getUnreadNotificationsCount(query);
       return handleResponse(res, result, { code: "NOTIFICATION_COUNT_FETCHED_SUCCESSFULLY" });
     } catch (error) {
