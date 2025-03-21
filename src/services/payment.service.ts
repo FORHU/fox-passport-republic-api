78e24d299a111fcb777913f52a2c0314a18e3fa4
KevinDaus7365 <kevin@seven365.com.sg>
2025-03-21 16:11:44 +0800

    Update payment.service.ts

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -71,7 +71,7 @@ export default class PaymentSvc {
             { status: account_status.COMPLETED, updatedAt: new Date() },
           );
           if (updateStripeAccount) {
-            await accountVerification(new ObjectId(userId));
+            await accountVerification(new ObjectId(userId), true);
           }
         }
         break;
