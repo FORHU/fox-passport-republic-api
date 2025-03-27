f047b79f083663a40b72d52fe65cae0f565326e5
KevinDaus7365 <kevin@seven365.com.sg>
2025-03-27 13:24:46 +0800

    Update announcement.service.ts
    build code for fetching multiple or single announcement document

--- src/services/announcement.service.ts
+++ src/services/announcement.service.ts
@@ -5,4 +5,12 @@ export default class AnnouncementSvc {
   static async createAnnouncement(data: TAnnouncement) {
     return await AnnouncementRepo.createAnnouncement(data);
   }
+
+  static async getAnnouncements(query: any, page: number, limit: number) {
+    return await AnnouncementRepo.getAnnouncements(query, page, limit);
+  }
+
+  static async getAnnouncement(query: any) {
+    return await AnnouncementRepo.getAnnouncement(query);
+  }
 }
