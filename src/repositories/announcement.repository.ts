85dbb2bd84d27f65b19dbfc94685e2883da1fcde
KevinDaus7365 <kevin@seven365.com.sg>
2025-03-27 13:24:07 +0800

    Update announcement.repository.ts
    build code for fetching announcements with pagination

--- src/repositories/announcement.repository.ts
+++ src/repositories/announcement.repository.ts
@@ -10,4 +10,23 @@ export default class AnnouncementRepo {
   static async createAnnouncement(data: TAnnouncement) {
     return await this.collection().insertOne(new MAnnouncement(data));
   }
+
+  static async getAnnouncements(query: any, page: number, limit: number) {
+    const skip = (page - 1) * limit;
+
+    const pipeline = [{ $match: query }, { $project: { attachment: 1, title: 1, description: 1, active: 1 } }, { $skip: skip }, { $limit: limit }];
+    const total_documents = await this.collection().countDocuments(query);
+    const result = await this.collection().aggregate(pipeline).toArray();
+
+    return {
+      data: result,
+      total_documents,
+      current_page: page,
+      total_pages: Math.ceil(total_documents / limit),
+    };
+  }
+
+  static async getAnnouncement(query: any) {
+    return await this.collection().findOne(query);
+  }
 }
