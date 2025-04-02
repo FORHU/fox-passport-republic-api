3cf9bb6c8c5e0be5aba872cd76104ec6304fe25a
joelralphdeveloper <163519284+joelralphdeveloper@users.noreply.github.com>
2025-04-02 17:28:17 +0800

    [API]: [BUG]: cannot get the notification realtime

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from "mongodb";
 import { TNotifications } from "../models/notification.model";
 import NotificationRepo from "../repositories/notification.repository";
+import UserSvc from "./user.service";
 
 export default class NotificationSvc {
   static async createNotification(data: TNotifications) {
@@ -9,8 +10,7 @@ export default class NotificationSvc {
   }
 
   static async getUnreadNotificationsCount(query: any) {
-    const result = await NotificationRepo.getUnreadNotificationsCount(query);
-    return result;
+    return await NotificationRepo.getUnreadNotificationsCount(query);
   }
 
   static async getOneNotification(query: any) {
@@ -25,10 +25,28 @@ export default class NotificationSvc {
 
   static async deleteNotifications(query: any) {
     try {
-      const result = await NotificationRepo.deleteNotifications(query);
-      return result;
+      return await NotificationRepo.deleteNotifications(query);
     } catch (error) {
       throw error;
+    }
+  }
+
+  static async getNotificationByRoomId(userId: string) {
+    try {
+      const user_id = new ObjectId(userId);
+      const userData = await UserSvc.getUser({ _id: user_id });
+      const query = {
+        receiver: user_id,
+        read: false,
+      };
+
+      const count = await this.getUnreadNotificationsCount(query);
+      return {
+        room_id: userData?.room_id,
+        count,
+      };
+    } catch (err) {
+      return err;
     }
   }
 }
